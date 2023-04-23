0308ecc2fedd78db871464e6c52300722463b583
us1802 <singh.umang1802@gmail.com>
2023-04-23 13:46:28 +0530

    tailwind set up

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className='font-semibold text-center mt-20 text-4xl'>Ram Shiv</div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
