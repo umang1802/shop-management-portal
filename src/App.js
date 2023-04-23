8333f6fd4675ce31e26b78a74c8dfd44439af6ef
Aakash <aakashsky0202@gmail.com>
2023-04-23 15:04:05 +0530

    Added Layout Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Main from './component/Main';
 
 function App() {
   return (
     <div className="App">
-      <div className='font-semibold text-center mt-20 text-4xl'>Ram Shiv</div>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      {/* <div className='font-semibold text-center text-4xl'>Ram Shiv</div> */}
+      <div className='mt-8'/>
+      <Main button1="Inactive products"/>
     </div>
   );
 }
