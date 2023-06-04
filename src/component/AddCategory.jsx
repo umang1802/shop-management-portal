24efb0d30b22f0a8d5db244e1ef15222e03cb0e1
us1802 <singh.umang1802@gmail.com>
2023-06-04 15:36:51 +0530

    api integration -1

--- src/component/AddCategory.jsx
+++ src/component/AddCategory.jsx
@@ -11,7 +11,7 @@ const AddCategory = () => {
     const fetchData = async () => {
       try {
         const response = await axios.get("https://shop-service-fo3n.onrender.com/get-categories");
-        setData(response.data);
+        setData(response.rows);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
