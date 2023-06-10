f7dfe2eca749423e9bbcc30dc6560b615ef8b9a5
Aakash <aakashsky0202@gmail.com>
2023-06-10 23:47:57 +0530

    Login Screen Changes

--- src/routes.js
+++ src/routes.js
@@ -6,20 +6,27 @@ import Products from './container/Products';
 import Offer from './container/Offer';
 import Bills from './container/Bills';
 import Expense from './container/Expense';
+import LoginScreen from './LoginScreen';
+import AuthContext from './authentication/AuthContext'
 
 
 const ShopRoutes = () => {
   return (
+    <AuthContext.Consumer>
+      {value => (
     <Layout>
       <Routes>
-        <Route  path="/" element={<Dashboard />} />
+        <Route  path="/" element={<LoginScreen />} />
         <Route path="/procuct" element={<Products />} />
         <Route path="/offer" element={<Offer />} />
         <Route path="/bills" element={<Bills />} />
         <Route path="/expense" element={<Expense />} />
         {/* Add more routes here */}
       </Routes>
-    </Layout>
+    </Layout> 
+    )}
+    </AuthContext.Consumer>
+
   );
 };
 
